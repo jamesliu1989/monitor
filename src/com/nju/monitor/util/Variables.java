03d26d7ebfbedf548065e6e1d655fe09139352f4
james <jamesliu1989@hotmail.com>
2016-08-11 00:00:06 +0800

    add static variable

---
+++ src/com/nju/monitor/util/Variables.java
@@ -0,0 +1,11 @@
+package com.nju.monitor.util;
+
+/**
+ * Created by james on 2016/8/10.
+ */
+public class Variables {
+
+    public static final double ERROR_TMP = 6553.5;
+
+    public static final double ERROR_HU = 255;
+}
